2572e5ca1f193a3845984867d7384734f64837ed
Hellen Barbosa <hellenbarbosa@Braejans-MacBook-Air.local>
2022-02-23 19:04:58 -0500

    first commit

---
+++ src/app/components/img/img.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit, Input , Output, EventEmitter} from '@angular/core';
+
+@Component({
+  selector: 'app-img',
+  templateUrl: './img.component.html',
+  styleUrls: ['./img.component.scss']
+})
+export class ImgComponent implements OnInit {
+  @Input() img : string  = '';
+  @Output() loaded = new EventEmitter<string>();
+  imageDefault = './assets/images/imagen-no-disponible.png';
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  imgError(){
+    this.img = this.imageDefault;
+  }
+  imgLoaded(){
+    console.log("Log hijo")
+    this.loaded.emit(this.img);
+  }
+}
